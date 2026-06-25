d9cc0f1dbe09c47e025c4edbf21ea248022719ed
Parishkrat Mishra <parishkrat.mishra@quencil.in>
2026-06-26 00:23:20 +0530

    Initial commit -  auth filepath

--- src/server.js
+++ src/server.js
@@ -8,10 +8,14 @@ import chatRouter from "./Routes/chatRoutes.js";
 import logger from "./utils/logger.js";
 import http from "http";
 import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // create HTTP server
 const server = http.createServer(app);
 
@@ -54,7 +58,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // routes
 app.use("/api", router);
